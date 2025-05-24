023f196a4595b981b6b01e5d80bd9e3ae52ae74f
Utku Yaşar <utkuyasar2003@icloud.com>
2025-05-24 20:42:28 +0300

    notification infrastructure has been created

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import NotificationExample from './components/NotificationExample';
 
 export default function App() {
   return (
-    <View >
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <NotificationExample />
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+});
