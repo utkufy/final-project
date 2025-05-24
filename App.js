af149b73fa732cc0e7ad9d91968b0d326fe4dc46
Utku Yaşar <utkuyasar2003@icloud.com>
2025-05-24 20:26:49 +0300

    Added sensors basic functions

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Sensors from './components/Sensors';
 
 export default function App() {
   return (
-    <View >
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <Sensors />
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
+});
